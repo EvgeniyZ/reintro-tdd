7e19e308a9b4a18d992566e7036c0fcebd68bbfe
EvgeniyZ <rocketjump07@gmail.com>
2018-02-22 22:21:12 +0300

    Make Fraction struct, add ToString, operator +.
    Last one test is failing.

--- src/Fraction.cs
+++ src/Fraction.cs
@@ -2,7 +2,31 @@
 
 namespace src
 {
-    public class Class1
+    public struct Fraction
     {
+        // above line
+        public int Nominator { get; }
+        // below line
+        public int Denominator { get; }
+        public Fraction(int nominator, int denominator)
+        {
+            Nominator = nominator;
+            Denominator = denominator;
+        }
+
+        public static Fraction operator +(Fraction a, Fraction b) 
+        {
+            if (a.Denominator == b.Denominator) 
+            {
+                return new Fraction(1, a.Denominator / 2);
+            }
+            var difference = b.Denominator - a.Denominator;
+            return new Fraction(b.Nominator + difference, b.Denominator);
+        }
+
+        public override string ToString() 
+        {
+            return $"{Nominator}/{Denominator}";
+        }
     }
 }
--- tests/FractionsTests.cs
+++ tests/FractionsTests.cs
@@ -1,4 +1,5 @@
 using System;
+using src;
 using Xunit;
 
 namespace FractionsUnitTests
@@ -11,6 +12,12 @@ namespace FractionsUnitTests
             var a = new Fraction(1, 2);
             var b = new Fraction(1, 4);
             Assert.Equal(new Fraction(3, 4), a + b);
+            a = new Fraction(1, 4);
+            b = new Fraction(1, 4);
+            Assert.Equal(new Fraction(1, 2), a + b);
+            a = new Fraction(3, 7);
+            b = new Fraction(1, 2);
+            Assert.Equal(new Fraction(13, 14), a + b);
         }
     }
 }
